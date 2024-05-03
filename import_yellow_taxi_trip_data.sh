252589577c2594abafa1a52b5d4ec707e0e849b0
pratikrathore8 <76628577+pratikrathore8@users.noreply.github.com>
2024-05-03 14:42:52 -0700

    Changed loading to directly use csv files + added notebook for falkon preprocessing

--- import_yellow_taxi_trip_data.sh
+++ import_yellow_taxi_trip_data.sh
@@ -6,8 +6,8 @@ yellow_schema="(vendor_id, tpep_pickup_datetime, tpep_dropoff_datetime, passenge
 
 yellow_schema_pre_2011="(vendor_id, tpep_pickup_datetime, tpep_dropoff_datetime, passenger_count, trip_distance, pickup_longitude, pickup_latitude, rate_code_id, store_and_fwd_flag, dropoff_longitude, dropoff_latitude, payment_type, fare_amount, extra, mta_tax, tip_amount, tolls_amount, total_amount)"
 
-for parquet_filename in data/yellow_tripdata*.parquet; do
-  [[ $parquet_filename =~ $year_month_regex ]]
+for csv_filename in data/yellow_tripdata*.csv; do
+  [[ $csv_filename =~ $year_month_regex ]]
   year=${BASH_REMATCH[1]}
 
   if [ $year -lt 2011 ]; then
@@ -16,10 +16,6 @@ for parquet_filename in data/yellow_tripdata*.parquet; do
     schema=$yellow_schema
   fi
 
-  echo "`date`: converting ${parquet_filename} to csv"
-  ./setup_files/convert_parquet_to_csv.R ${parquet_filename}
-
-  csv_filename=${parquet_filename/.parquet/.csv}
   cat $csv_filename | psql nyc-taxi-data -c "COPY yellow_tripdata_staging ${schema} FROM stdin CSV HEADER;"
   echo "`date`: finished raw load for ${csv_filename}"
 
@@ -29,3 +25,27 @@ for parquet_filename in data/yellow_tripdata*.parquet; do
   rm -f $csv_filename
   echo "`date`: deleted ${csv_filename}"
 done;
+
+# for parquet_filename in data/yellow_tripdata*.parquet; do
+#   [[ $parquet_filename =~ $year_month_regex ]]
+#   year=${BASH_REMATCH[1]}
+
+#   if [ $year -lt 2011 ]; then
+#     schema=$yellow_schema_pre_2011
+#   else
+#     schema=$yellow_schema
+#   fi
+
+#   echo "`date`: converting ${parquet_filename} to csv"
+#   ./setup_files/convert_parquet_to_csv.R ${parquet_filename}
+
+#   csv_filename=${parquet_filename/.parquet/.csv}
+#   cat $csv_filename | psql nyc-taxi-data -c "COPY yellow_tripdata_staging ${schema} FROM stdin CSV HEADER;"
+#   echo "`date`: finished raw load for ${csv_filename}"
+
+#   psql nyc-taxi-data -f setup_files/populate_yellow_trips.sql
+#   echo "`date`: loaded trips for ${csv_filename}"
+
+#   rm -f $csv_filename
+#   echo "`date`: deleted ${csv_filename}"
+# done;
